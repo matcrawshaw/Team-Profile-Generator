8716443787e6883c516eaa7a17e8f45a83abc89d
matcrawshaw <mat_crawshaw@hotmail.com>
2023-03-06 16:56:24 +0000

    got application to output team array and populate new html file

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const Employee = require("./lib/Employee");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -14,4 +13,105 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-const tay = new Engineer('tay', 101, 'email@email.com', 'taync');
+
+const getName =  {
+        type: 'input', 
+        message: 'What is the Employee\'s Name?',
+        name: 'name',
+            };
+
+const getId = {
+        type: 'input', 
+        message: 'What is the Employee ID?',
+        name: 'ID',
+            };
+
+const getEmail = {
+        type: 'input', 
+        message: 'What is their Email?',
+        name: 'email',
+            };
+ const getOfficeNum = {
+        type: 'input', 
+        message: 'What is the Manager\'s Office Number?',
+        name: 'officeNum',
+            };             
+const getGitUser =  {
+        type: 'input', 
+        message: 'What is the Engineer\'s GitHub Username?',
+        name: 'gitUsername',
+                };
+const getSchool =  {
+        type: 'input', 
+        message: 'What is the Intern\'s School?',
+        name: 'school',
+                };
+
+const menuChoice =  {
+        type: 'list', 
+        message: 'Team member added! Please choose from the below:',
+        choices: ['Add an Engineer', 'Add an Intern', 'Finish Building the Team'],
+        name: 'nextChoice'
+                        };
+       
+const currentTeam = []
+
+
+const newManager = () => {
+    inquirer.prompt([getName, getId, getEmail, getOfficeNum, menuChoice])
+    .then((response) => {
+        // const manager = generateMarkdown(response);
+    const employee = new Manager(response.name, response.ID, response.email, response.officeNum)
+    currentTeam.push(employee);
+  
+    nextPrompt(response.nextChoice);
+    });
+}
+
+const nextPrompt = (answer) => {
+    switch(answer) {
+case 'Add an Engineer': newEngineer(); 
+break;
+case 'Add an Intern': newIntern();
+break;
+case 'Finish Building the Team':
+    generateTeamPage(currentTeam);
+    }
+}
+
+const newEngineer = () => {
+    inquirer.prompt([getName, getId, getEmail, getGitUser, menuChoice])
+    .then((response) => {
+        // const manager = generateMarkdown(response);
+    const employee = new Engineer(response.name, response.ID, response.email, response.gitUsername)
+    currentTeam.push(employee);
+    nextPrompt(response.nextChoice);
+    });
+}
+const newIntern = () => {
+    inquirer.prompt([getName, getId, getEmail, getSchool, menuChoice])
+    .then((response) => {
+        // const manager = generateMarkdown(response);
+    const employee = new Intern(response.name, response.ID, response.email, response.school)
+    currentTeam.push(employee);
+    nextPrompt(response.nextChoice);
+    });
+}
+        
+
+
+
+const init = () => {
+
+
+}
+
+newManager();
+
+
+const generateTeamPage = (currentTeam) => {
+    fs.writeFile("team.html", render(currentTeam), (error) => error ? console.error(error) : console.log('Success!'));
+}
+//  
+
+
